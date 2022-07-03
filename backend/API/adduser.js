89ceea2ef6c633d7a7bf9e9d15903be6a368a78f
Rickyxrc <rickyxrc@outlook.com>
2022-07-03 16:38:10 +0800

    add docker support.

---
+++ backend/API/adduser.js
@@ -0,0 +1,17 @@
+let db = require('../db/index')
+
+exports.get = (req, res) => {
+    if (req.query['API_KEY'] != process.env.API_KEY) {
+        res.send('invalid API_KEY.');
+        return 0;
+    }
+    else {
+        var sql = 'INSERT INTO names (name) VALUES ' + req.query['data'] + ' ;'
+        db.query(sql, (err, data) => {
+            if (err) {
+                return res.send('error:' + err.message);
+            }
+            res.send('OK');
+        })
+    }
+}
